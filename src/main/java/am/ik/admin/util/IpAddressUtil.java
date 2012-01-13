c646c0490044222333ef5c310d3500154e7bbe9d
Toshiaki Maki <makingx@gmail.com>
2012-01-13 21:53:50 +0900

    added ping

--- src/main/java/am/ik/admin/util/IpAddressUtil.java
+++ src/main/java/am/ik/admin/util/IpAddressUtil.java
@@ -1,8 +1,14 @@
 package am.ik.admin.util;
 
+import org.apache.commons.lang.SystemUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
 public class IpAddressUtil {
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(IpAddressUtil.class);
+
     public static String ipToHex(String ip) {
         String[] bits = ip.split("\\.");
         Assert.isTrue(bits.length == 4);
@@ -23,5 +29,16 @@ public class IpAddressUtil {
             sb.append(Integer.parseInt(hex.substring(i * 2, (i + 1) * 2), 16));
         }
         return sb.toString();
+    }
+
+    public static boolean ping(String ip) {
+        String[] command = { "ping", SystemUtils.IS_OS_WINDOWS ? "-n" : "-c",
+                "1", ip };
+        try {
+            return new ProcessBuilder(command).start().waitFor() == 0;
+        } catch (Exception e) {
+            LOGGER.warn("failed to ping " + ip, e);
+            return false;
+        }
     }
 }
